fbe2195aca4e087e6d61f41986b106f2b1e3e428
SinaLiri <sina.liri@yahoo.com>
2023-03-28 14:39:02 +0430

    project-complete

--- Components/layout/Layout.js
+++ Components/layout/Layout.js
@@ -1,20 +1,33 @@
 import Link from "next/link";
 import React from "react";
+import { Menubar } from 'primereact/menubar';
+import { InputText } from 'primereact/inputtext';
+//theme
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+    
+//core
+import "primereact/resources/primereact.min.css";
+
+//icons
+import "primeicons/primeicons.css";                                         
+//primeflex
+import "/node_modules/primeflex/primeflex.css"
 import style from "./layout.module.scss";
 
 const Layout = ({ children }) => {
+  const items = [ 
+];
+const start = <Link href='/' ><h3 className="flex align-items-center" >Cargovo </h3> <h5 className="">Choose your Car</h5> </Link>;
+    const end = <InputText placeholder="Search" type="text" className="w-full" />;
   return (
     <>
-      <header className={`grid ${style.headerContainer}`}>
-        <Link href="/">
-          <h2 className={`col-12 ${style.mainBrand}`}>Cargovo</h2>
-          <span></span>
-          <p className={`col-12 ${style.maintext}`}>Choose your car</p>
-          <span></span>
-        </Link>
+      <header className={`grid w-full`}>
+      <div className={`col-10 col-offset-1 card  ${style.headerContainer}`}>
+            <Menubar style={{backgroundColor:"#b1e0be"}} model={items} start={start} end={end} />
+        </div>
       </header>
       <div className={style.container}>{children}</div>
-      <footer className={`c0l-12 ${style.footer}`}>
+      <footer className={`col- ${style.footer}`}>
         footer &copy;
       </footer>
     </>
